b03e315b1fcf3717a0197d0b5b68ed139389d0f2
Tatiana Barga <tatibspersonal@gmail.com>
2024-01-10 00:09:48 -0600

    joke set up is pulled and displayed on load

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,21 @@
+import HearAJoke from '../components/HearAJoke';
+
 function Home() {
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Welcome Home!</h1>
-    </div>
+    <>
+      <div
+        className="text-center d-flex flex-column justify-content-center align-content-center"
+        style={{
+          height: '90vh',
+          padding: '30px',
+          maxWidth: '400px',
+          margin: '0 auto',
+        }}
+      >
+        <HearAJoke />
+      </div>
+
+    </>
   );
 }
 
